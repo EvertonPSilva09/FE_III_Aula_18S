bbdab9935ed26f1f8ebee1d8e8b3c80f96a3871f
Everton Silva <everton.p.silva@outlook.com>
2023-03-28 21:49:38 -0300

    Add BaseTemplate to homepage

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,10 @@
-export default function HomePage(){
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+import BaseTemplate from "../../templates/base-template";
+export default function HomePage() {
+  return (
+    <BaseTemplate>
+      <div>
+        <h1>Home Page</h1>
+      </div>
+    </BaseTemplate>
+  );
 }
