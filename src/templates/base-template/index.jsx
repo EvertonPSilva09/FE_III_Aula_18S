df783d9049b9c4d6c86c306ff774ec1d96d4f7ac
Everton Silva <everton.p.silva@outlook.com>
2023-03-28 21:38:18 -0300

    Add header on base-template

--- src/templates/base-template/index.jsx
+++ src/templates/base-template/index.jsx
@@ -1,6 +1,9 @@
+import Header from "../../components/header";
+
 export default function BaseTemplate() {
   return (
     <div>
+      <Header />
       <h1>Meu Template Base</h1>
     </div>
   );
