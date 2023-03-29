457635b34192c547893550630f1ebd11ff54bbc7
Everton Silva <everton.p.silva@outlook.com>
2023-03-28 22:11:54 -0300

    Add contato page and include BaseTemplate

---
+++ src/pages/contato/index.jsx
@@ -0,0 +1,11 @@
+import BaseTemplate from "../../templates/base-template";
+
+export default function ContatoPage() {
+  return (
+    <BaseTemplate>
+      <div>
+        <h1>Contato - Fale conosco</h1>
+      </div>
+    </BaseTemplate>
+  );
+}
